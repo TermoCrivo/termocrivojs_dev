102fac9769aaab8cc99483448bcd090a57974e5f
20171014040038 <gesio.m@academico.ifrn.edu.br>
2020-09-24 18:12:32 -0300

    tentativa falha

--- src/termocrivo.js
+++ src/termocrivo.js
@@ -1,9 +1,20 @@
-// Define a global variable 'Module' with a method 'onRuntimeInitialized':
-Module = {
-    onRuntimeInitialized() {
-        // this is our application:
-        console.log(cv.getBuildInformation())
-    }
+let imgElement = document.getElementById('imageSrc');
+let inputElement = document.getElementById('fileInput');
+inputElement.addEventListener('change', (e) => {
+    imgElement.src = URL.createObjectURL(e.target.files[0]);
+}, false);
+imgElement.onload = function () {
+    let src = cv.imread('canvasInput');
+    let dst = new cv.Mat();
+    let gray = new cv.Mat();
+
+    // gray and threshold image
+    cv.cvtColor(src, gray, cv.COLOR_RGBA2GRAY, 0);
+    cv.threshold(gray, gray, 0, 255, cv.THRESH_BINARY_INV + cv.THRESH_OTSU);
+
+    cv.imshow('canvasOutput', gray);
+    src.delete(); dst.delete(); gray.delete();
+};
+function onOpenCvReady() {
+    document.getElementById('status').innerHTML = 'OpenCV.js is ready.';
 }
-// Load 'opencv.js' assigning the value to the global variable 'cv'
-cv = require('./opencv.js')
