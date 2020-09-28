3753469f094e964b0eb052dc7d978a15da1a7637
20171014040038 <gesio.m@academico.ifrn.edu.br>
2020-09-28 16:01:26 -0300

    contagem de pretos funcionando! Falta calcular a porcentagem!

--- src/crivo.js
+++ src/crivo.js
@@ -4,22 +4,52 @@ import cores from './cores.js'
 export default function crivo(mascaras) {
 
     let somaPixel = []
-    let preto = 0
-    for (let i in mascaras) {
-        for (let j in mascaras[i].rows) {
-            for (let k in mascaras[i].cols)
-                if (mascaras[i].isContinuous()) {
-                    //let H = mascaras[i].data[j * mascaras[i].cols * mascaras[i].channels() + k * mascaras[i].channels()];
-                    //let S = mascaras[i].data[j * mascaras[i].cols * mascaras[i].channels() + k * mascaras[i].channels() + 1];
-                    let V = mascaras[i].data[j * mascaras[i].cols * mascaras[i].channels() + k * mascaras[i].channels() + 2]
-                    if (V == 0) {
-                        somaPixel[i] = preto++
+
+    mascaras.forEach(matriz => {
+        let preto = 3
+        for (let i = 0; i < matriz.rows; i++) {
+            for (let j = 0; j < matriz.cols; j++) {
+                if (matriz.isContinuous()) {
+                    let v = matriz.data[i * matriz.cols * matriz.channels() + j + matriz.channels() + 2]
+                    if (v == 0) {
+                        preto++
                     }
                 }
+            }
         }
-    }
+        somaPixel.push(preto)
+    })
 
     console.log(somaPixel)
+
+
+    let row = 2, col = 3
+    if (mascaras[5].isContinuous()) {
+        let H = mascaras[5].data[row * mascaras[5].cols * mascaras[5].channels() + col * mascaras[5].channels()]
+        let S = mascaras[5].data[row * mascaras[5].cols * mascaras[5].channels() + col * mascaras[5].channels() + 1]
+        let V = mascaras[5].data[row * mascaras[5].cols * mascaras[5].channels() + col * mascaras[5].channels() + 2]
+        let A = mascaras[5].data[row * mascaras[5].cols * mascaras[5].channels() + col * mascaras[5].channels() + 3]
+
+        console.log(`Pixels: ${mascaras[5].cols * mascaras[5].rows}`)
+        console.log(`HSV - A: ${typeof H}, ${S}, ${V} - ${A} `)
+        // console.log(`Mat.cols: ${ mascaras[5].cols } Mat.channels: ${ mascaras[5].channels() } `)
+        console.log(`${row} * ${mascaras[5].cols} * ${mascaras[5].channels()} + ${col}* ${mascaras[5].channels()} `)
+    }
+
+
+    // for (let i in mascaras) {
+    //     for (let j in mascaras[i].rows) {
+    //         for (let k in mascaras[i].cols)
+    //             if (mascaras[i].isContinuous()) {
+    //                 //let H = mascaras[i].data[j * mascaras[i].cols * mascaras[i].channels() + k * mascaras[i].channels()];
+    //                 //let S = mascaras[i].data[j * mascaras[i].cols * mascaras[i].channels() + k * mascaras[i].channels() + 1];
+    //                 let V = mascaras[i].data[j * mascaras[i].cols * mascaras[i].channels() + k * mascaras[i].channels() + 2]
+    //                 if (V == 0) {
+    //                     somaPixel[i] = preto++
+    //                 }
+    //             }
+    //     }
+    // }
 
     let res = {
         vermelhoPorcento: 0,
